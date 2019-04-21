6ffaa1f336a3381cdea83c87958bf14ea385b0bf
mufengshaonian <haolinwuyu@sina.com>
2019-04-21 15:01:53 +0800

    第一次上课作业----一周饮食得代码

---
+++ food_list0418.py
@@ -0,0 +1,17 @@
+days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
+for day in days:
+	if day == 'Monday':
+		print(day + " I eat apple.")
+	elif day =='Tuesday':
+		print(day + " I eat banana.")
+	elif day == 'Wednesday':
+		print(day + " I eat tomato.")
+	elif day == 'Thursday':
+		print(day + " I eat potato.")
+	elif day == 'Friday':
+		print(day + " I eat rice.")
+	elif day == 'Saturday':
+		print(day + " I eat egg.")
+	else:
+		print(day + " I eat meat.")
+	
