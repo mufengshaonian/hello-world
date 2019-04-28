5c93f261886e4e4beac8bdbf2a6fdc9f0f7a4dcb
mufengshaonian <haolinwuyu@sina.com>
2019-04-28 10:11:48 +0800

    调整了文档位置

--- practise0415.py
+++
@@ -1,5 +0,0 @@
-Python 3.6.8 (tags/v3.6.8:3c6b436a57, Dec 24 2018, 00:16:47) [MSC v.1916 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> print("Today is Monday, I want to have a good rest.")
-Today is Monday, I want to have a good rest.
->>> 
